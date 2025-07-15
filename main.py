ce8eeaf648ec432fc08cff3d2745e01b6624c5b4
m0n6005e <m0n6005e@gmail.com>
2025-07-15 12:07:10 -0600

    Player ship

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Starting Asteroids!")
+from constants import *
+import pygame
+from player import Player
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+    while True:                                
+        for event in pygame.event.get():       
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black", rect=None, special_flags=0)
+        player.draw(screen)
+        pygame.display.flip()
+       
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
